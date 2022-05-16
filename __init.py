f1fe205595dc3885053fd132b3a7d6418062764b
puseyr <pusey.ronald@gmail.com>
2022-05-16 08:30:14 -0400

    2nd version of .py file

--- __init.py
+++ __init.py
@@ -3,4 +3,14 @@
 # Date: 5/16/2022
 # Usage: Download my favorite linux distros + other ISOs as well
 
+
+# Sites to scrape from:
+# https://distrowatch.com/
+# Should I go direct to the sites?
+
+# For parsing a web page
 from bs4 import BeautifulSoup
+
+# For grabbing and using the ISOs via .torrent files
+import qbittorrent
+
