e0d8b2bea075a512742b486ef0ece6a708337981
s0nlxaftrsh0ck <s0nlxaftrsh0ck@gmail.com>
2022-05-29 21:08:11 -0400

    Updating files

--- __init.py
+++ __init.py
@@ -6,11 +6,74 @@
 
 # Sites to scrape from:
 # https://distrowatch.com/
-# Should I go direct to the sites?
+# Should I get ISOs direct from the sites they originated from??
 
 # For parsing a web page
+from cgitb import html
 from bs4 import BeautifulSoup
 
 # For grabbing and using the ISOs via .torrent files
 import qbittorrent
 
+# For converting / downloading the page needed for scraping
+import requests
+
+# For using system functions / read and write files
+import os
+
+
+
+page = requests.get("https://distrowatch.com")
+
+page_content = page.text
+
+with open('distroWatch.html', 'w', encoding='utf-8') as fp:
+    fp.write(page_content)
+
+# print(page.status_code) # Received Status code 200 @ https://distrowatch.com
+
+# print(page.content)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# print(soup.prettify())
+
+# print(list(soup.children))
+
+# BeautifulSoup Objects for DistroWatch
+# [0] = DocType
+# [1] = Tags
+# [2] = NavigableString
+print([type(item) for item in list(soup.children)])
+
+# Put the tags into the html 
+html = list(soup.children)[1]
+
+print("\Tag List created\n")
+
+# Print out the tag list
+print(list(html.children))
+
+print("\nBody created...\n")
+
+body = list(html.children)[0]
+
+print("\nPrinting body\n")
+
+print(list(body.children))
+
+print("\nGrabbing paragraphs\n")
+
+p = list(body.children)[1]
+
+print("\nPrinting Text from paragraphs\n")
+
+print(p.get_text())
+
+print("\n--->End<---\n")
+
+# Finding all instances of a tag at once
+
+all_option = soup.find_all('option')[0].get_text()
+
+print(all_option)
